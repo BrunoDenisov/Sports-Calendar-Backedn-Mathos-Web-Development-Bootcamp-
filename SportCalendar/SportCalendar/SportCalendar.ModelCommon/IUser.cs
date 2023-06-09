7dd67a46a794fff76ad9c6d1fe8f97167008f90a
Sebastian Tkalčić <stkalcic00@gmail.com>
2023-06-09 13:52:29 +0200

    created interface/classes for User, Role, installed Npgsql to SportCalendarRepository

---
+++ SportCalendar/SportCalendar/SportCalendar.ModelCommon/IUser.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCalendar.ModelCommon
+{
+    public interface IUser
+    {
+        Guid Id { get; set; }
+        string FirstName { get; set; }
+        string LastName { get; set; }
+        string Password { get; set; }
+        string Email { get; set; }
+        Guid RoleId { get; set; }
+        bool IsActive { get; set; }
+        Guid UpdatedByUserId { get; set; }
+        DateTime DateCreated { get; set; }
+        DateTime DateUpdated { get; set; }
+    }
+}
