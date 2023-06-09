41492b6c9629bfff1f456e071abc9f63a9b380fd
Sebastian Tkalčić <stkalcic00@gmail.com>
2023-06-09 09:04:55 +0200

    created new project, naming issue created ContainerConfig for DI

---
+++ SportCalendar/SportCalendar/SportCalendar.WebApi/App_Start/ContainerConfig.cs
@@ -0,0 +1,28 @@
+﻿using Autofac;
+using Autofac.Integration.WebApi;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Web;
+using System.Web.Http;
+
+namespace SportCalendar.WebApi.App_Start
+{
+    public class ContainerConfig
+    {
+        public static void ConfigureContainer()
+        {
+
+            ContainerBuilder builder = new ContainerBuilder();
+            builder.RegisterApiControllers(Assembly.GetExecutingAssembly());
+
+            // register interfaces
+
+            Autofac.IContainer container = builder.Build();
+
+            GlobalConfiguration.Configuration.DependencyResolver = new AutofacWebApiDependencyResolver(container);
+        }
+    }
+}
--- SportCalendar/SportEvent/SportEvent.WebApi/Areas/HelpPage/ModelDescriptions/DictionaryModelDescription.cs
+++
@@ -1,6 +0,0 @@
-namespace SportEvent.WebApi.Areas.HelpPage.ModelDescriptions
-{
-    public class DictionaryModelDescription : KeyValuePairModelDescription
-    {
-    }
-}
