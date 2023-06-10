e04fd46a8ec5a8c2394827d45c3216165205a37a
DESKTOP-JN8TE8V\Bracurda <filip.tod95@gmail.com>
2023-06-10 18:41:03 +0200

    ﻿initial commit  task #6

---
+++ SportCalendar/SportCalendar/SportCalendar.Service/CountyService.cs
@@ -0,0 +1,20 @@
+﻿using SportCalendar.Model;
+using SportCalendar.ModelCommon;
+using SportCalendar.RepositoryCommon;
+using SportCalendar.ServiceCommon;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SportCalendar.Service
+{
+    public class CountyService : ICountyService
+    {
+       
+
+        
+
+    }
+}
