1d42428df73308b99d8334d074891022079f2cdc
Bruno Denisov <bruno.denisov9@gmail.com>
2023-06-16 15:24:24 +0200

    Commit

--- SportCalendar/SportCalendar/SportCalendar.Service/EventSponsorService.cs
+++ SportCalendar/SportCalendar/SportCalendar.Service/EventSponsorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using SportCalendar.ServiceCommon;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace SportCalendar.Service
 {
-    public class EventSponsorService
+    public class EventSponsorService : IEventSponsorService
     {
     }
 }
