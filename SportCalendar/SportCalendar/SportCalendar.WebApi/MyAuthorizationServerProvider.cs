ad870d43364e55bbf679f2b91dd35391fef9fb19
Sebastian Tkalčić <stkalcic00@gmail.com>
2023-06-27 13:57:47 +0200

    added additional user information: id, username, role to token

--- SportCalendar/SportCalendar/SportCalendar.WebApi/MyAuthorizationServerProvider.cs
+++ SportCalendar/SportCalendar/SportCalendar.WebApi/MyAuthorizationServerProvider.cs
@@ -1,9 +1,12 @@
-﻿using Microsoft.Owin.Security.OAuth;
+﻿using System;
+using Microsoft.Owin.Security.OAuth;
 using SportCalendar.Common;
 using SportCalendar.Model;
 using SportCalendar.Repository;
+using System.Collections.Generic;
 using System.Security.Claims;
 using System.Threading.Tasks;
+using Microsoft.Owin.Security;
 
 namespace SportCalendar.WebApi
 {
@@ -13,6 +16,16 @@ namespace SportCalendar.WebApi
         public override async Task ValidateClientAuthentication(OAuthValidateClientAuthenticationContext context)
         {
             context.Validated();
+        }
+
+        public override Task TokenEndpoint(OAuthTokenEndpointContext context)
+        {
+            foreach (KeyValuePair<string, string> property in context.Properties.Dictionary)
+            {
+                context.AdditionalResponseParameters.Add(property.Key, property.Value);
+            }
+
+            return Task.FromResult<object>(null);
         }
         public override async Task GrantResourceOwnerCredentials(OAuthGrantResourceOwnerCredentialsContext context)
         {
@@ -28,17 +41,26 @@ namespace SportCalendar.WebApi
                     context.SetError("invalid_grant", "Provided username and password is incorrect");
                     return;
                 }
+
                 var identity = new ClaimsIdentity(context.Options.AuthenticationType);
                 identity.AddClaim(new Claim("Id", user.Id.ToString()));
                 identity.AddClaim(new Claim(ClaimTypes.Name, user.Username));
                 identity.AddClaim(new Claim(ClaimTypes.Role, user.Access));
                 identity.AddClaim(new Claim("Email", user.Email));
-                context.Validated(identity);
+
+                AuthenticationProperties properties = new AuthenticationProperties(new Dictionary<string, string>
+                {
+                    { "Id", user.Id.ToString() },
+                    { "Username", user.Username },
+                    { "Role", user.Access }
+                });
+
+                context.Validated(new AuthenticationTicket(identity, properties));
                 return;
             }
             context.SetError("Invalid Username or Password");
             return;
-            
+
 
         }
     }
